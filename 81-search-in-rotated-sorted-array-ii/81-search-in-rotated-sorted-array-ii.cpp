e2ae40ce057f29e1cecd4532584936775a0f4b56
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-03-28 18:26:37 +0530

    Time: 31 ms (5.16%), Space: 14.1 MB (32.08%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,8 +1,32 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
-        for(auto i : nums)
-            if(i == target) return true;
-        return false;
+    bool search(vector<int>& A, int t) {
+        if (A.empty()) 
+            return false;
+        
+        int l = 0, r = A.size() - 1;
+        
+        while (l < r) {
+            int m = l + (r - l) / 2;    
+            if (A[m] == t) return true;
+            if (A[l] < A[m]) {
+                if (A[l] <= t && t < A[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else if (A[m] < A[r]) {
+                if (A[m] < t && t <= A[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                if (A[l] == A[m]) l++;
+                if (A[m] == A[r]) r--;
+            }
+        }
+        
+        return A[l] == t? true : false;
     }
 };
