21ad64034e201235e948a9e48b77a6e4eb239d10
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-10 12:07:32 +0530

    Time: 8 ms (27.41%), Space: 5.9 MB (73.42%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x==0) return x;
+        int start = 1, end = x;
+        while (start <= end){
+            int mid = start + (end-start)/2;
+            if(mid == x/mid)
+                return mid;
+            if(mid>x/mid)
+                end = mid -1;
+            else {
+                start = mid + 1;
+            }
+        }
+        return end;
+
+    }
+};
