fff80aae42b6abb8009103c1168bfd9805334f3a
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-13 23:19:24 +0530

    Time: 5 ms (43.24%), Space: 16.8 MB (8.25%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>>recSub(vector<int>nums, int i, vector<int>proc){
-        vector<vector<int>> power;
+//     vector<vector<int>>recSub(vector<int>nums, int i, vector<int>proc){
+//         vector<vector<int>> power;
+//         if(i==nums.size()){
+//             power.push_back(proc);
+//             return power;
+//         }
+//         int num = nums[i];
+//         vector<vector<int>> left = recSub(nums, i+1, proc);
+//         proc.push_back(num);
+//         vector<vector<int>> right = recSub(nums, i+1, proc);
+//         right.insert(right.end(), left.begin(), left.end());
+        
+//         return right;
+//     }
+    void recSub(vector<int>nums, int i, vector<int>proc, vector<vector<int>>&power){
         if(i==nums.size()){
             power.push_back(proc);
-            return power;
+            return;
         }
-        int num = nums[i];
-        vector<vector<int>> left = recSub(nums, i+1, proc);
-        proc.push_back(num);
-        vector<vector<int>> right = recSub(nums, i+1, proc);
-        right.insert(right.end(), left.begin(), left.end());
-        
-        return right;
+        proc.push_back(nums[i]);
+        recSub(nums, i+1, proc, power);
+        proc.pop_back();
+        recSub(nums, i+1, proc, power);
     }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        int i = 0;
         vector<int>proc;
+        vector<vector<int>>power;
         // int n = nums.size(), p = 1 << n;
         // vector<vector<int>> subs(p);
         // for (int i = 0; i < p; i++) {
@@ -29,6 +38,7 @@ public:
         // }
         // return subs;
         
-        return recSub(nums, i, proc);
+        recSub(nums, 0, proc, power);
+        return power;
     }
 };
