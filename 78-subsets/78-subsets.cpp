6c2ca6fbe8f7017221f77c4b219b0ae6a6146d18
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-14 00:45:41 +0530

    Time: 7 ms (34.37%), Space: 7 MB (87.22%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -25,18 +25,16 @@ public:
     //     recSub(nums, i+1, proc, power);
     // }
     
-    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
-        subs.push_back(sub);
-        for (int j = i; j < nums.size(); j++) {
-            sub.push_back(nums[j]);
-            subsets(nums, j + 1, sub, subs);
-            sub.pop_back();
-        }
-    }
+    // void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+    //     subs.push_back(sub);
+    //     for (int j = i; j < nums.size(); j++) {
+    //         sub.push_back(nums[j]);
+    //         subsets(nums, j + 1, sub, subs);
+    //         sub.pop_back();
+    //     }
+    // }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>proc;
-        vector<vector<int>>power;
         // int n = nums.size(), p = 1 << n;
         // vector<vector<int>> subs(p);
         // for (int i = 0; i < p; i++) {
@@ -46,10 +44,25 @@ public:
         //         }
         //     }
         // }
-        // return subs;
         
-        //recSub(nums, 0, proc, power);
-        subsets(nums, 0, proc, power);
-        return power;
+        vector<vector<int>> subs;
+        subs.push_back({});
+        for(auto num : nums){
+            int n = subs.size();
+            for(int i = 0; i<n; i++){
+                subs.push_back(subs[i]); // 1st pass -> {} is pushed again
+                subs.back().push_back(num); //{1} in the new {}, current num passed
+                // 2nd pass
+                // {} created and pushed, 2 added, {1} created and pushed 2 added, 
+                // size increases by a factor of 2;
+            }
+        }
+        return subs;
+        
+        // vector<int>proc;
+        // vector<vector<int>>power;
+        // recSub(nums, 0, proc, power);
+        // subsets(nums, 0, proc, power);
+        // return power;
     }
 };
