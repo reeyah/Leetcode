1a6f56326da0e6704db1b5da854247fe87f7073f
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-14 10:21:24 +0530

    Time: 4 ms (31.11%), Space: 6.5 MB (83.12%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rec(string digits, string proc, int i, vector<string>&letters, const vector<string>&chars){
+        if(i==digits.length()){
+            letters.push_back(proc);
+            return;
+        }
+            
+        int d = digits[i] - '0'; 
+        for(char c: chars[d-2])
+            rec(digits, proc+c, i+1, letters, chars);
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return {};
+        vector<string>chars = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string>letters;
+        string proc;
+
+        rec(digits, proc, 0, letters, chars);
+        
+        return letters;
+    }
+};
