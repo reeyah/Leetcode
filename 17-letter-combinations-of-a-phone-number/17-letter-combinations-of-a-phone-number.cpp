357d84f425e01c1de4b95d5e7b551d44b51295a6
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-14 10:35:16 +0530

    Time: 6 ms (14.06%), Space: 6.6 MB (57.43%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    void rec(string digits, string proc, int i, vector<string>&letters, const vector<string>&chars){
-        if(i==digits.length()){
-            letters.push_back(proc);
-            return;
-        }
+//     void rec(string digits, string proc, int i, vector<string>&letters, const vector<string>&chars){
+//         if(i==digits.length()){
+//             letters.push_back(proc);
+//             return;
+//         }
             
-        int d = digits[i] - '0'; 
-        for(char c: chars[d-2])
-            rec(digits, proc+c, i+1, letters, chars);
-    }
+//         int d = digits[i] - '0'; 
+//         for(char c: chars[d-2])
+//             rec(digits, proc+c, i+1, letters, chars);
+//     }
     
     vector<string> letterCombinations(string digits) {
-        if(digits.empty())
-            return {};
-        vector<string>chars = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        vector<string>letters;
-        string proc;
+//         if(digits.empty())
+//             return {};
+//         vector<string>chars = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+//         vector<string>letters;
+//         string proc;
 
-        rec(digits, proc, 0, letters, chars);
+//         rec(digits, proc, 0, letters, chars);
+        
+        vector<string> pad = {"abc", "def", "ghi", "jkl","mno", "pqrs", "tuv", "wxyz"};
+    
+        if (digits.empty()) return {};
+		vector<string> letters;
+        letters.push_back("");
+        
+        for(auto digit: digits) {
+            vector<string> tmp; //re-initialize
+            for(auto c: pad[digit - '0' - 2]) {
+                for(auto s: letters) {
+                    tmp.push_back(s + c); //add the new c to every string s in letter
+                }
+            }
+            letters.swap(tmp);
+        }
         
         return letters;
     }
