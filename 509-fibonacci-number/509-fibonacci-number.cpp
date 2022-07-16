6cc7c57ec9e8e2add820468e5ffc02f3465028dd
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-16 21:32:37 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.57%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
+    int fib(int n, vector<int>&dp){
+        if(n < 2){
+            dp[n] = n;
+            return dp[n];
+        }
+        if(dp[n] != -1)
+            return dp[n];
+        dp[n] = fib(n-1, dp) + fib(n-2, dp);
+        
+        return dp[n];
+    }
+    
     int fib(int n) {
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
-        return fib(n-1)+fib(n-2);
+        // RECURSION
+        // if(n==0)
+        //     return 0;
+        // if(n==1)
+        //     return 1;
+        // return fib(n-1)+fib(n-2);
+        
+        // MEMOIZATION
+        vector<int>dp(n+1, -1);
+        fib(n, dp);
+        return dp[n];
     }
 };
