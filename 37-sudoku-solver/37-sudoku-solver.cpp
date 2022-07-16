a7a5d278058b84cb75eb9cc1df94fbec7e1c07ee
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-16 15:53:40 +0530

    Time: 10 ms (94.82%), Space: 6.4 MB (50.35%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,5 +1,5 @@
 class Solution {
-        int row[9][10], col[9][10], cube[3][3][10];
+        int row[9][9], col[9][9], cube[3][3][9];
 public:
 //     bool isSafe(vector<vector<char>>& board, int r, int c, char ch){
 //         for(int i = 0; i<9; i++){
@@ -16,7 +16,7 @@ public:
      
     //Hash check - reduce complexity
     bool check(int r, int c, char ch) {
-        if(row[r][ch - '0'] || col[c][ch - '0'] || cube[r/3][c/3][ch - '0'] ) 
+        if(row[r][ch - '1'] || col[c][ch - '1'] || cube[r/3][c/3][ch - '1'] ) 
             return false;
         return true;
     }
@@ -31,11 +31,11 @@ public:
         for(char ch = '1'; ch<='9'; ch++){ //will try put every no. in every column
             if(check(r, c, ch)){
                 board[r][c] = ch;
-                row[r][ch - '0'] = 1; col[c][ch - '0'] = 1; cube[r/3][c/3][ch - '0'] = 1;
+                row[r][ch - '1'] = 1; col[c][ch - '1'] = 1; cube[r/3][c/3][ch - '1'] = 1;
                 if(isSolved(board, r, c+1))  //crucial check
                     return true;
                 board[r][c] = '.';
-                row[r][ch - '0'] = 0; col[c][ch - '0'] = 0; cube[r/3][c/3][ch - '0'] = 0;
+                row[r][ch - '1'] = 0; col[c][ch - '1'] = 0; cube[r/3][c/3][ch - '1'] = 0;
             }
         }
         
@@ -49,9 +49,9 @@ public:
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
                 if (board[r][c] != '.') {
-                    row[r][board[r][c] - '0'] = 1;
-                    col[c][board[r][c] - '0'] = 1;
-                    cube[r/3][c/3][board[r][c] - '0'] = 1;
+                    row[r][board[r][c] - '1'] = 1;
+                    col[c][board[r][c] - '1'] = 1;
+                    cube[r/3][c/3][board[r][c] - '1'] = 1;
                 } 
             }
         }
