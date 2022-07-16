a774ba6e2fef3af6694bac0cb226a0ed6a22890c
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-16 15:52:27 +0530

    Time: 16 ms (92.13%), Space: 6.4 MB (50.35%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,17 +1,26 @@
 class Solution {
+        int row[9][10], col[9][10], cube[3][3][10];
 public:
-    bool isSafe(vector<vector<char>>& board, int r, int c, char ch){
-        for(int i = 0; i<9; i++){
-            if(board[r][i] == ch) //row check
-                return false;
-            if(board[i][c] == ch) //column check
-                return false;
-                if(board[r-r%3+i/3][c-c%3+i%3]==ch) //cube check
-                    return false;
-        }
+//     bool isSafe(vector<vector<char>>& board, int r, int c, char ch){
+//         for(int i = 0; i<9; i++){
+//             if(board[r][i] == ch) //row check
+//                 return false;
+//             if(board[i][c] == ch) //column check
+//                 return false;
+//                 if(board[r-r%3+i/3][c-c%3+i%3]==ch) //cube check
+//                     return false;
+//         }
         
+//         return true;
+//     }
+     
+    //Hash check - reduce complexity
+    bool check(int r, int c, char ch) {
+        if(row[r][ch - '0'] || col[c][ch - '0'] || cube[r/3][c/3][ch - '0'] ) 
+            return false;
         return true;
     }
+    
     bool isSolved(vector<vector<char>>& board, int r = 0, int c = 0){
         if(r==9) 
             return true;
@@ -20,11 +29,13 @@ public:
         if(board[r][c] != '.')
             return isSolved(board, r, c+1);
         for(char ch = '1'; ch<='9'; ch++){ //will try put every no. in every column
-            if(isSafe(board, r, c, ch)){
+            if(check(r, c, ch)){
                 board[r][c] = ch;
-                if(isSolved(board, r, c+1)) 
+                row[r][ch - '0'] = 1; col[c][ch - '0'] = 1; cube[r/3][c/3][ch - '0'] = 1;
+                if(isSolved(board, r, c+1))  //crucial check
                     return true;
                 board[r][c] = '.';
+                row[r][ch - '0'] = 0; col[c][ch - '0'] = 0; cube[r/3][c/3][ch - '0'] = 0;
             }
         }
         
@@ -32,6 +43,18 @@ public:
     }
     
     void solveSudoku(vector<vector<char>>& board) {
+        memset(row, 0, sizeof(row));
+        memset(col, 0, sizeof(col));
+        memset(cube, 0, sizeof(cube));
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] != '.') {
+                    row[r][board[r][c] - '0'] = 1;
+                    col[c][board[r][c] - '0'] = 1;
+                    cube[r/3][c/3][board[r][c] - '0'] = 1;
+                } 
+            }
+        }
         isSolved(board);
     }
 };
