7fd06d527cc02eb90b20ae69d0d530626e25ce80
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-16 23:26:44 +0530

    Time: 90 ms (68.82%), Space: 48.4 MB (77.65%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int reach=0, n=nums.size();
-        for (int i = 0; i < n && i <= reach; ++i){
+        for (int i = 0; i < n && i <= reach; ++i)
             reach = max(i + nums[i], reach);
-            if(reach>=n-1)
-                return true;
-        }
-        return false;
+        return reach >= n-1;
     }
 };
