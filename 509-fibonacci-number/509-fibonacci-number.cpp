b94c019ab957a051c4e1a72dea215bff5d282432
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-16 21:42:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.74%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -25,12 +25,24 @@ public:
         // vector<int>dp(n+1, -1);
         // fib(n, dp);
         
-        vector<int>dp(n+1);
-        dp[0] = 0;
-        if(n>0)
-            dp[1] = 1;
-        for(int i=2; i<=n; i++)
-            dp[i] = dp[i-1] + dp[i-2];
-        return dp[n];
+        //TABULATION
+        // vector<int>dp(n+1);
+        // dp[0] = 0;
+        // if(n>0)
+        //     dp[1] = 1;
+        // for(int i=2; i<=n; i++)
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // return dp[n];
+        if(n<2)
+            return n;
+        
+        int a = 0, b = 1, sum = 0;
+        for(int i = 2; i<=n; i++){
+            sum = a + b;
+            a = b;
+            b = sum;
+        }
+        
+        return sum;
     }
 };
