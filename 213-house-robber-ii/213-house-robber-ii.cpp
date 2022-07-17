82250b4fb56e1a5c9cea8814563394e1fd1eeb26
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-17 08:48:42 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (97.58%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int helpRob(vector<int>&nums){
+    int helpRob(vector<int>&nums, int l, int r){
         if(nums.size()==0)
             return 0;
         int prev1=0, prev2=0;
-        for(auto num: nums){
-            int curr = max(prev1, prev2+num);
+        for(int i = l; i<=r; i++){
+            int curr = max(prev1, prev2+nums[i]);
             prev2 = prev1;
             prev1 = curr;
         }
@@ -16,13 +16,6 @@ public:
     int rob(vector<int>& nums) {
         if(nums.size() == 1)
             return nums[0];
-        vector<int>temp1, temp2;
-        for(int i =0; i<nums.size(); i++){
-            if(i!=nums.size()-1)
-                temp2.push_back(nums[i]);
-            if(i!=0)
-                temp1.push_back(nums[i]);
-        }
-        return max(helpRob(temp1), helpRob(temp2));
+        return max(helpRob(nums, 0, nums.size()-2), helpRob(nums, 1, nums.size()-1));
     }
 };
