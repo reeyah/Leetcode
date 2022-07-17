7b91673dae03fe853f5deb3ed6690872612ca382
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-17 12:41:42 +0530

    Time: 12 ms (35.42%), Space: 8.6 MB (65.77%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     
-    int calc(vector<vector<int>>& triangle, int n, int r, int c, vector<vector<int>>& dp){
-        if(r==n-1)
-            return triangle[r][c];
-        if(dp[r][c]!=-1)
-            return dp[r][c];
-        int mini=triangle[r][c];
-        int m = triangle[r].size();
-        mini = mini+min(calc(triangle, n, r+1, c, dp),calc(triangle, n, r+1, c+1, dp));
+    //MEMOIATION
+//     int calc(vector<vector<int>>& triangle, int n, int r, int c, vector<vector<int>>& dp){
+//         if(r==n-1)
+//             return triangle[r][c];
+//         if(dp[r][c]!=-1)
+//             return dp[r][c];
+//         int mini=triangle[r][c];
+//         int m = triangle[r].size();
+//         mini = mini+min(calc(triangle, n, r+1, c, dp),calc(triangle, n, r+1, c+1, dp));
         
-        return dp[r][c] = mini;
+//         return dp[r][c] = mini;
         
-    }
+//     }
     
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>>dp(n, vector<int>(triangle.back().size(), -1));
-        // for (int layer = n-2; layer >= 0; layer--) {
-        //     for (int i = 0; i <= layer; i++) {
-        //         minlen[i] = min(minlen[i], minlen[i+1]) + triangle[layer][i]; 
-        //     }
-        // }
-        // return minlen[0];
+        vector<int>minlen(triangle.back());
+        // vector<vector<int>>dp(n, vector<int>(triangle.back().size(), -1));
+        for (int layer = n-2; layer >= 0; layer--) {
+            for (int i = 0; i <= layer; i++) {
+                minlen[i] = min(minlen[i], minlen[i+1]) + triangle[layer][i]; 
+            }
+        }
+        return minlen[0];
         
-        return calc(triangle, triangle.size(), 0, 0, dp);
+//         return calc(triangle, triangle.size(), 0, 0, dp);
     }
 };
