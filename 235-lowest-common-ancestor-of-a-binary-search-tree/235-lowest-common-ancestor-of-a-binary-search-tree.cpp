644de1d932f224e4ea4eb128233594ac822856d8
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-19 01:08:02 +0530

    Time: 65 ms (10.62%), Space: 23.3 MB (63.67%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -13,7 +13,7 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if((p->val<=root->val && q->val>=root->val) || (p->val>=root->val && q->val<=root->val))
             return root;
-        if(p->val<=root->val && q->val<=root->val)
+        if(p->val<root->val && q->val<root->val)
             return lowestCommonAncestor(root->left, p, q);
         return lowestCommonAncestor(root->right, p, q);
     }
