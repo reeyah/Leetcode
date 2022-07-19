c35720db342809350b65948ef682ac2bcefffee9
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-07-20 02:38:25 +0530

    Time: 28 ms (84.03%), Space: 13.5 MB (83.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    bool bipartiteBFS(int i, vector<int>&color, vector<vector<int>>&graph){
-        queue<int>q;
-        q.push(i);
-        color[i]=1;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
+//     bool bipartiteBFS(int i, vector<int>&color, vector<vector<int>>&graph){
+//         queue<int>q;
+//         q.push(i);
+//         color[i]=1;
+//         while(!q.empty()){
+//             int node = q.front();
+//             q.pop();
             
-            for(auto it:graph[node]){
-                if(color[it]==-1){
-                    color[it]=1-color[node];
-                    q.push(it); 
-                }else if(color[it]==color[node])
+//             for(auto it:graph[node]){
+//                 if(color[it]==-1){
+//                     color[it]=1-color[node];
+//                     q.push(it); 
+//                 }else if(color[it]==color[node])
+//                     return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+    
+    bool bipartiteDFS(int node, vector<int>&color, vector<vector<int>>&graph){
+        if(color[node]==-1)
+            color[node]=1;
+            
+        for(auto it:graph[node]){
+            if(color[it]==-1){
+                color[it]=1-color[node];
+                if(!bipartiteDFS(it, color, graph))
                     return false;
-            }
+            }else if(color[it]==color[node])
+                return false;
         }
         
         return true;
@@ -25,7 +41,7 @@ public:
         vector<int> color(V, -1);
         for(int i = 0; i<V; i++){
 	        if(color[i]==-1)
-	            if(!bipartiteBFS(i, color, graph))
+	            if(!bipartiteDFS(i, color, graph))
 	                return false;
 	    }
 	    
