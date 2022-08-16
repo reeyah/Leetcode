031e0c5460884cc3251d7f5aa2228c9f810260de
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-08-16 12:05:27 +0530

    Time: 20 ms (96.69%), Space: 19.3 MB (57.58%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int snowBallSize = 0; 
-        for (int i=0;i<nums.size();i++){
-	        if (nums[i]==0){
-                snowBallSize++; 
-            }
-            else if (snowBallSize > 0) {
-                nums[i-snowBallSize]=nums[i];
-	            nums[i]=0;
-	            
+        for (int i=0, j=0;i<nums.size();i++){
+	      if(nums[i]&&i==j)//skip nonzero elements
+                j++;
+            else if(nums[i]){
+                nums[j++]=nums[i]; 
+                nums[i]=0;
             }
         }
     }
