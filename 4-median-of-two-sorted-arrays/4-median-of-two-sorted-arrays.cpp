ab54e2b206b2e2fb0677c6655ce7390aaa7bd43b
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-08-25 23:33:14 +0530

    Time: 40 ms (91.49%), Space: 89.3 MB (61.65%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        if(n1<n2)
-            return findMedianSortedArrays(nums2, nums1);
-        int lo = 0, hi = n2*2;
-        while(lo <= hi){
-            int mid2 = lo + (hi-lo)/2, mid1 = n1+n2-mid2;
+    double findMedianSortedArrays(vector<int>& a1, vector<int>& a2) {
+        if (a1.size() > a2.size()) swap(a1, a2); // make sure a1 is shorter
+        
+        int n1 = a1.size(), n2 = a2.size();
+        
+        // range of a1 cut location: n1 means no right half for a1
+        int lo = 0, hi = n1;
+        while (lo <= hi) {
+            int cut1 = (lo + hi)/2; // cut location is counted to right half
+            int cut2 = (n1 + n2)/2 - cut1;
             
-            double l1 = (mid1==0)? INT_MIN : nums1[(mid1-1)/2],
-                l2 = (mid2==0)? INT_MIN : nums2[(mid2-1)/2],
-                r1 = (mid1 == n1*2)? INT_MAX : nums1[mid1/2],
-                r2 = (mid2 == n2*2)? INT_MAX : nums2[mid2/2];
+            int l1 = cut1 == 0? INT_MIN : a1[cut1-1];
+            int l2 = cut2 == 0? INT_MIN : a2[cut2-1];
+            int r1 = cut1 == n1? INT_MAX : a1[cut1];
+            int r2 = cut2 == n2? INT_MAX : a2[cut2];
             
-            if(l1>r2) lo = mid2+1;
-            else if(l2>r1) hi = mid2-1;
-            
-            else return (max(l1,l2) + min(r1,r2)) / 2;
+            if (l1 > r2) hi = cut1-1;
+            else if (l2 > r1) lo = cut1+1;
+            else return (n1+n2)%2? min(r1,r2) : (max(l1,l2) + min(r1,r2))/2.;
         }
         return -1;
     }
