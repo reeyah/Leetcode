1018a4786a1e00a567f615671492c7979880a163
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-08-25 22:24:04 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (67.89%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,16 +4,13 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int high = n, low = 1;
-        while(low<high){
-            int mid = low + (high-low)/2;
-            if(!isBadVersion(mid)){
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
+        int lo = 1, hi = n;
+        while(lo < hi){
+            int mid = lo + (hi-lo)/2;
+            if(isBadVersion(mid))
+                hi = mid;
+            else lo = mid + 1;
         }
-        
-        return low;
+        return lo;
     }
 };
