740dacd3b5df1482a38d70f4c992b89a9b978f28
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-08-26 16:41:05 +0530

    Time: 12 ms (62.98%), Space: 13.7 MB (17.85%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
+    int binary(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        while (l <= r) {
+            int mid = (r-l)/2+l;
+            if (nums[mid] < target)
+                l = mid+1;
+            else
+                r = mid-1;
+        }
+        return l;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int it1 = std::lower_bound(nums.begin(), nums.end(), target)  - nums.begin();
-        int it2 = std::upper_bound(nums.begin(), nums.end(), target)  - nums.begin();
-        //cout<<it2;
-        if (it1<=it2 && it1 < nums.size() && nums[it1] == target) 
-            return {it1, it2-1};
-        return {-1, -1};
+        int idx1 = binary(nums, target);
+        int idx2 = binary(nums, target+1)-1;
+        if (idx1 < nums.size() && nums[idx1] == target)
+            return {idx1, idx2};
+        else
+            return {-1, -1};
     }
 };
