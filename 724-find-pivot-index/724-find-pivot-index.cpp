9b979ac42ecba62037364fb05a7ad3dd70699d12
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-08-27 23:37:49 +0530

    Time: 47 ms (32.41%), Space: 31.1 MB (87.76%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-//         int sumLeft = 0, sumRight = 0, i=0, j = nums.size()-1;
-//         while(i<j){
-//             if(sumLeft == sumRight){
-//                 sumLeft+= nums[i++];
-//                 sumRight+= nums[j--];
-//             }
-            
-//             else if(sumLeft > sumRight)
-//                 sumRight += nums[j--];
-//             else sumLeft += nums[i++];
-//         }
-        
-//         if(sumLeft == sumRight)
-//             return i;
-//         return -1;
-        
-        int sum = 0, preSum =0;
-        for(int num : nums) sum += num;
-        
-        for(int i=0; i<nums.size(); preSum += nums[i++])
-            if(preSum * 2 == sum - nums[i])
+        int tSum = accumulate(nums.begin(), nums.end(), 0);
+        int pSum = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(pSum * 2 == tSum-nums[i])
                 return i;
-            
+            pSum += nums[i];
+        }
         return -1;
     }
 };
