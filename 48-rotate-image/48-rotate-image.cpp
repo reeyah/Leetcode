31088292b0e98c6e025d1c94ff5aabfa16954ada
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-08-31 00:22:44 +0530

    Time: 7 ms (17.34%), Space: 7.3 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,11 +3,9 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
         vector<vector<int>> res(n, vector<int>(n));
-        for(int j=0; j<n; j++){
-            for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+            for(int i=0; i<n; i++)
                 res[i][j] = matrix[n-1-j][i];
-            }
-        }
         matrix = res;
     }
 };
