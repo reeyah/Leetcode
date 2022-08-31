391f2167ddebbf6423b145049061d5ed210fc0a0
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-01 02:48:24 +0530

    Time: 83 ms (40.72%), Space: 19.1 MB (44.61%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     void dfs(vector<vector<int>>&vis, int i, vector<vector<int>>&heights, int j){
+        if(vis[i][j] == 1)
+            return;
+        
         int m = heights.size(), n = heights[0].size();
         vis[i][j]=1;
         
-        if (i-1 >= 0 && !vis[i-1][j] && heights[i-1][j] >= heights[i][j]) 
+        if (i-1 >= 0 && heights[i-1][j] >= heights[i][j]) 
             dfs(vis, i-1, heights, j);
-        if (i+1 < m && !vis[i+1][j] && heights[i+1][j] >= heights[i][j]) 
+        if (i+1 < m && heights[i+1][j] >= heights[i][j]) 
             dfs(vis, i+1, heights, j);
-        if (j-1 >= 0 && !vis[i][j-1] && heights[i][j-1] >= heights[i][j]) 
+        if (j-1 >= 0 && heights[i][j-1] >= heights[i][j]) 
             dfs(vis, i, heights, j-1);
-        if (j+1 < n && !vis[i][j+1] && heights[i][j+1] >= heights[i][j]) 
+        if (j+1 < n && heights[i][j+1] >= heights[i][j]) 
             dfs(vis, i, heights, j+1);
     }
     
