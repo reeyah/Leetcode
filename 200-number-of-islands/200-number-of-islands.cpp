cf2e2b2704219bcb72f4318ba2bb6f2519709e3d
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-08-31 22:48:13 +0530

    Time: 105 ms (8.40%), Space: 17.9 MB (19.63%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,27 +1,39 @@
 class Solution {
 public:
-    void markIslands(vector<vector<char>>& grid, int rows, int cols, int i, int j){
-        if(i<0 || j<0 || i>=rows || j>=cols || grid[i][j] != '1')
-            return;
-        grid[i][j] = '2';
+    // DFS
+//     void markIslands(vector<vector<char>>& grid, int rows, int cols, int i, int j){
+//         if(i<0 || j<0 || i>=rows || j>=cols || grid[i][j] != '1')
+//             return;
+//         grid[i][j] = '2';
         
-        markIslands(grid, rows, cols, i-1, j);
-        markIslands(grid, rows, cols, i+1, j);
-        markIslands(grid, rows, cols, i, j-1);
-        markIslands(grid, rows, cols, i, j+1);
-    }
+//         markIslands(grid, rows, cols, i-1, j);
+//         markIslands(grid, rows, cols, i+1, j);
+//         markIslands(grid, rows, cols, i, j-1);
+//         markIslands(grid, rows, cols, i, j+1);
+//     }
     
     int numIslands(vector<vector<char>>& grid) {
-        int rows = grid.size(), cols = grid[0].size();
+        int rows = grid.size(), cols = grid[0].size(),numOfIslands = 0, offsets[]={0,1,0,-1,0};
         if(rows == 0)
             return 0;
         
-        int numOfIslands = 0;
         for(int i=0; i<rows; i++)
             for(int j=0; j<cols; j++)
                 if(grid[i][j] == '1'){
-                    markIslands(grid, rows, cols, i, j);
+                    // markIslands(grid, rows, cols, i, j);
                     numOfIslands++;
+                    stack<pair<int,int>>coords;
+                    coords.push(make_pair(i,j));
+                    while(!coords.empty()){
+                        int x = coords.top().first, y = coords.top().second;
+                        grid[x][y]='2';
+                        coords.pop();
+                        for(int k=0; k<4; k++){
+                            int r = x + offsets[k], c = y + offsets[k+1];
+                            if(r>=0 && c>=0 && r<rows && c<cols && grid[r][c] == '1')
+                                coords.push(make_pair(r,c));
+                        }
+                    }
                 }
         return numOfIslands;
     }
