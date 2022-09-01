3f14626b2a02c6ccfb77f837748aace112228467
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-02 01:04:20 +0530

    Time: 280 ms (8.75%), Space: 86.3 MB (54.67%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,22 +11,23 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, int &count, int high){
-        if(!root)
-            return;
-        if(root->val >= high){
-            count++;
-            high = root->val;
-        }
+//     void dfs(TreeNode* root, int &count, int high){
+//         if(!root)
+//             return;
+//         if(root->val >= high){
+//             count++;
+//             high = root->val;
+//         }
         
-        dfs(root->left, count, high);
-        dfs(root->right, count, high);   
-    }
-    int goodNodes(TreeNode* root) {
-        if(!root)
-            return 0;
-        int count = 0;
-        dfs(root, count, root->val);
-        return count;
+//         dfs(root->left, count, high);
+//         dfs(root->right, count, high);   
+//     }
+    int goodNodes(TreeNode* root, int ma = -10000) {
+        // if(!root)
+        //     return 0;
+        // int count = 0;
+        // dfs(root, count, root->val);
+        // return count;
+        return root ? goodNodes(root->left, max(ma, root->val)) + goodNodes(root->right, max(ma, root->val)) + (root->val >= ma) : 0;
     }
 };
