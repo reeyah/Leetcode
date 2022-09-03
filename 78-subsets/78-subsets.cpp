767c6bc804ea66036d0f6f95df02a35635c30332
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-03 10:01:31 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (87.57%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,68 +1,20 @@
 class Solution {
 public:
-//     vector<vector<int>>recSub(vector<int>nums, int i, vector<int>proc){
-//         vector<vector<int>> power;
-//         if(i==nums.size()){
-//             power.push_back(proc);
-//             return power;
-//         }
-//         int num = nums[i];
-//         vector<vector<int>> left = recSub(nums, i+1, proc);
-//         proc.push_back(num);
-//         vector<vector<int>> right = recSub(nums, i+1, proc);
-//         right.insert(right.end(), left.begin(), left.end());
-        
-//         return right;
-//     }
-    // void recSub(vector<int>nums, int i, vector<int>proc, vector<vector<int>>&power){
-    //     if(i==nums.size()){
-    //         power.push_back(proc);
-    //         return;
-    //     }
-    //     proc.push_back(nums[i]);
-    //     recSub(nums, i+1, proc, power);
-    //     proc.pop_back();
-    //     recSub(nums, i+1, proc, power);
-    // }
-    
-    // void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
-    //     subs.push_back(sub);
-    //     for (int j = i; j < nums.size(); j++) {
-    //         sub.push_back(nums[j]);
-    //         subsets(nums, j + 1, sub, subs);
-    //         sub.pop_back();
-    //     }
-    // }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
-        // int n = nums.size(), p = 1 << n;
-        // vector<vector<int>> subs(p);
-        // for (int i = 0; i < p; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if ((i >> j) & 1) {
-        //             subs[i].push_back(nums[j]);
-        //         }
-        //     }
-        // }
-        
-        vector<vector<int>> subs;
-        subs.push_back({});
-        for(auto num : nums){
-            int n = subs.size();
-            for(int i = 0; i<n; i++){
-                subs.push_back(subs[i]); // 1st pass -> {} is pushed again
-                subs.back().push_back(num); //{1} in the new {}, current num passed
-                // 2nd pass
-                // {} created and pushed, 2 added, {1} created and pushed 2 added, 
-                // size increases by a factor of 2;
-            }
+    void dfs(vector<int>& nums, int i, vector<vector<int>>&subs, vector<int>&sub){
+        if(i==nums.size()){
+            subs.push_back(sub);
+            return;
         }
-        return subs;
+        sub.push_back(nums[i]);
+        dfs(nums, i+1, subs, sub);
+        sub.pop_back();
+        dfs(nums, i+1, subs, sub);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>subs;
+        vector<int>sub;
+        dfs(nums, 0, subs, sub);
         
-        // vector<int>proc;
-        // vector<vector<int>>power;
-        // recSub(nums, 0, proc, power);
-        // subsets(nums, 0, proc, power);
-        // return power;
+        return subs;
     }
 };
