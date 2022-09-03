2a332dacbf1e1ac6ed1c0217097001a9dcf987d7
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-04 02:05:04 +0530

    Time: 236 ms (30.14%), Space: 41 MB (81.20%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,22 +1,38 @@
 class MyHashSet {
 public:
-    vector<int>m;
+    vector<list<int>>m;
     int size;
     MyHashSet() {
-        size = 1e6+1;
+        size = 107;
         m.resize(size);
     }
     
+    int hash(int key){
+        return key%size;
+    }
+    
+    list<int>::iterator search(int key){
+        int i = hash(key);
+        return find(m[i].begin(), m[i].end(), key);
+    }
+    
     void add(int key) {
-        m[key]=1;
+        if(contains(key))
+            return;
+        int i = hash(key);
+        m[i].push_back(key);
     }
     
     void remove(int key) {
-        m[key]=0;
+        if(!contains(key))
+            return;
+        int i = hash(key);
+        m[i].erase(search(key));
     }
     
     bool contains(int key) {
-        return m[key];
+        int i = hash(key);
+        return search(key) != m[i].end();
     }
 };
 
