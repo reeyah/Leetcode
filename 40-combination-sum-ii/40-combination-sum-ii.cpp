dcb3059fd5dac90ac3beff5f0f8c5fc5fd565777
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-05 00:25:59 +0530

    Time: 13 ms (40.14%), Space: 10.5 MB (98.36%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+        void rec(vector<vector<int>>&ans, vector<int>&s, vector<int>&candidates, int target, int i){
+        if(target == 0){
+            ans.push_back(s);
+            return;
+        }
+        if(target < 0)
+            return;
+        for(int j=i; j<candidates.size(); j++){
+            if(j!=i && candidates[j]==candidates[j-1])
+                continue;
+            s.push_back(candidates[j]);
+            rec(ans, s, candidates, target - candidates[j], j+1);
+            s.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>>ans;
+        vector<int>s;
+        rec(ans, s, candidates, target, 0);
+        return ans;
+    }
+};
