33d0656cbe7a3620322a311b31d9f846fcdc121b
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-04 11:03:16 +0530

    Time: 5 ms (75.50%), Space: 13.6 MB (32.63%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,29 +12,26 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        todo.push({root, {0,0}});
-        while(!todo.empty()){
-            auto p = todo.front();
-            todo.pop();
-            TreeNode* node = p.first;
+        map<int,map<int,multiset<int>>> store;
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        q.push({root, {0,0}});
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            TreeNode *curr = p.first;
             int x = p.second.first, y = p.second.second;
-            nodes[x][y].insert(node->val);
-            if(node->left)
-                todo.push({node->left, {x-1, y+1}});
-            if(node->right)
-                todo.push({node->right, {x+1, y+1}});
+            store[x][y].insert(curr->val);
+            if(curr->left)
+                q.push({curr->left, {x-1, y+1}});
+            if(curr->right)
+                q.push({curr->right, {x+1, y+1}});
         }
-        
-        vector<vector<int>>ans;
-        for(auto i: nodes){
-            vector<int>col;
-            for(auto j: i.second)
-                col.insert(col.end(), j.second.begin(), j.second.end());
-            ans.push_back(col);
+        vector<vector<int>>res;
+        for(auto i:store){
+            res.push_back({});
+            for(auto j:i.second)
+                res.back().insert(res.back().end(), j.second.begin(), j.second.end());
         }
-        
-        return ans;
+        return res;
     }
 };
