0f8dbd6f4f271c845068dfcd9aa141eb5c187d4b
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-05 07:18:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (53.95%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-//     void rec(string digits, string proc, int i, vector<string>&letters, const vector<string>&chars){
-//         if(i==digits.length()){
-//             letters.push_back(proc);
-//             return;
-//         }
-            
-//         int d = digits[i] - '0'; 
-//         for(char c: chars[d-2])
-//             rec(digits, proc+c, i+1, letters, chars);
-//     }
-    
-    vector<string> letterCombinations(string digits) {
-//         if(digits.empty())
-//             return {};
-//         vector<string>chars = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-//         vector<string>letters;
-//         string proc;
-
-//         rec(digits, proc, 0, letters, chars);
-        
-        vector<string> pad = {"abc", "def", "ghi", "jkl","mno", "pqrs", "tuv", "wxyz"};
-    
-        if (digits.empty()) return {};
-		vector<string> letters;
-        letters.push_back("");
-        
-        for(auto digit: digits) {
-            vector<string> tmp; //re-initialize
-            for(auto c: pad[digit - '0' - 2]) {
-                for(auto s: letters) {
-                    tmp.push_back(s + c); //add the new c to every string s in letter
-                }
-            }
-            letters.swap(tmp);
+    void dfs(string digits, string &s, vector<string>&ans, int i, const vector<string>&letters){
+        if(i==digits.size()){
+            ans.push_back(s);
+            return;
         }
-        
-        return letters;
+        for(char j: letters[digits[i]-2-'0']){
+            s.push_back(j);
+            dfs(digits, s, ans, i+1, letters);
+            s.pop_back();
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty())
+            return {};
+        vector<string>letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string>ans;
+        string s;
+        dfs(digits, s, ans, 0, letters);
+        return ans;
     }
 };
