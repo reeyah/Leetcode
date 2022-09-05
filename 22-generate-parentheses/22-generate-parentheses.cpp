342e4ef81deaecd8a481806ab26bf890119e6220
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-05 08:27:19 +0530

    Time: 0 ms (100.00%), Space: 11.4 MB (88.23%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    void rec(int n, int m, string s, vector<string>&ans){
+    void rec(int n, int m, string &s, vector<string>&ans){
         if(n==0 && m==0){
             ans.push_back(s);
             return;
         }
         
-        if(n>0)
-            rec(n-1, m, s+"(", ans);
-        if(m>n)
-            rec(n, m-1, s+")", ans);
+        if(n>0){
+            s+="(";
+            rec(n-1, m, s, ans);
+            s.pop_back();
+        }
+        if(m>n){
+            s+=')';
+            rec(n, m-1, s, ans);
+            s.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
