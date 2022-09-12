3b3b8ed65b753af66abed0704fa667c5fd647778
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-12 11:17:29 +0530

    Time: 7 ms (80.84%), Space: 10.6 MB (27.39%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end());
+        int score=0;
+        for(int i=0, j=tokens.size()-1; i<=j;){
+            // cout<<i<<","<<j<<" ";
+            if(power>=tokens[i]){
+                score++;
+                power-=tokens[i];
+                i++;
+            }
+            else{
+                if(i!=j && score>0 && power+tokens[j] >= tokens[i]){
+                    power+= tokens[j];
+                    score--;
+                    j--;
+                }
+                else i++;
+            }
+        }
+        return score;
+    }
+};
