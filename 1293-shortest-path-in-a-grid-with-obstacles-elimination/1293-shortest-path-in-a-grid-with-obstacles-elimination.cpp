28503c3391f0c86e812b56cccdfd90ec628f8472
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-09-15 13:37:27 +0530

    Time: 8 ms (97.24%), Space: 8.7 MB (92.67%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int rows=grid.size(), cols=grid[0].size();
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),-1));
-        queue<vector<int>> q;
+        int m = grid.size(), n =grid[0].size(), steps=1, shortest=m+n-2;
+        if(k>=shortest-1)
+            return shortest;
+        vector<int>dir = {0,1,0,-1,0};
+        vector<vector<int>>vis(m, vector<int>(n,-1));
+        queue<vector<int>>q;
+        q.push({0,0,k});
+        vis[0][0]=k;
         
-        q.push({0,0,0,k});
         while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            int x = it[0], y = it[1];
-            
-            if(x==rows-1 && y ==cols-1)
-                return it[2];
-            
-            if(x<0 || y<0 || x>=rows || y>=cols)
-                continue;
-            
-            if(grid[x][y]==1){
-                if(it[3]>0)
-                    it[3]--;
-                else continue;
+            int size = q.size();
+            while(size--){
+                auto v = q.front(); q.pop();
+                int x = v[0], y=v[1], left=v[2];
+                if(vis[x][y]>left)
+                    continue;
+                for(int k=0; k<4; k++){
+                    int x1 = x+dir[k], y1=y+dir[k+1], newLeft = left;
+                   // int dist = m-x1 + n-y1 -2;
+                    if(x1==m-1 && y1==n-1)
+                        return steps;
+                    if(x1<0 || y1<0 || x1>=m || y1>=n)
+                        continue;
+                    if(grid[x1][y1])
+                        newLeft--;
+                    if(vis[x1][y1]>=newLeft)
+                        continue;
+                    else vis[x1][y1] = newLeft;
+                    q.push({x1, y1, newLeft});
+                }       
             }
-            
-            if(vis[x][y]!=-1 && vis[x][y]>=it[3])
-                continue;
-            vis[x][y] = it[3];
-            
-            q.push({x+1, y, it[2]+1, it[3]});
-            q.push({x-1, y, it[2]+1, it[3]});
-            q.push({x, y+1, it[2]+1, it[3]});
-            q.push({x, y-1, it[2]+1, it[3]});
+            steps++;
         }
-        
         return -1;
     }
 };
