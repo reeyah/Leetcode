d9315b68e3551ccf32a23fcf5cfebd36589adf92
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-10-02 16:39:24 +0530

    Time: 30 ms (75.89%), Space: 8.4 MB (59.18%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -3,15 +3,14 @@ public:
     int numRolls(int n, int k, int target, vector<vector<int>>&dp, int res = 0){
         if (n == 0 || target == 0) 
             return n == target;
-        if(dp[n][target])
-            return dp[n][target]-1;
+        if(dp[n][target] != -1)
+            return dp[n][target];
         for (auto i = 1; i <= k && i<=target; ++i)
             res = (res + numRolls(n - 1, k, target - i, dp)) % 1000000007;
-        dp[n][target]=res+1;
-        return res;
+        return dp[n][target]=res;
     }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1, vector<int>(target+1, 0));
+        vector<vector<int>>dp(n+1, vector<int>(target+1, -1));
         return numRolls(n, k, target, dp);
     }
 };
