5bf70d9d0e3e326882c2cff72c1ef976a516228a
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-11-22 11:47:29 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.44%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,14 +1,20 @@
 class Solution {
+    bool isSquare(int n){
+        int sqrtN = (int)sqrt(n);
+        return sqrtN*sqrtN==n;
+    }
 public:
     int numSquares(int n) {
-        if(n<=0)
-            return 0;
-        vector<int>cntPerfectSqrs(n+1, INT_MAX);
-        cntPerfectSqrs[0]=0;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j*j<=i; j++)
-                cntPerfectSqrs[i]=min(cntPerfectSqrs[i], cntPerfectSqrs[i-j*j]+1);
-        }
-        return cntPerfectSqrs[n];
+        if(isSquare(n))
+            return 1;
+        while((n&3)==0)
+            n>>=2;
+        while((n&7)==7)
+            return 4;
+        int sqrtN = (int)sqrt(n);
+        for(int i = 1; i <= sqrtN; i++)
+            if (isSquare(n - i*i)) 
+                return 2;
+        return 3;
     }
 };
