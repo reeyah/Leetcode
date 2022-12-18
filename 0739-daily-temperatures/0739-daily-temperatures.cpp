491065e3d760ee976062b7238799065c9e2e5603
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2022-12-18 08:47:12 +0530

    Time: 396 ms (59.59%), Space: 102.9 MB (49.40%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
         stack<int>s;
-        s.push(0);
         vector<int>warm(n,0);
         for(int i=0; i<n; i++){
             while(!s.empty() && temperatures[i]>temperatures[s.top()])
