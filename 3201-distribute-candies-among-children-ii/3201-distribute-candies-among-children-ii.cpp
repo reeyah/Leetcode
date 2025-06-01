ea3d6f9b8b84dd3fce977c05f3b86a30abac6469
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-01 23:20:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (57.06%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    long long distributeCandies(int n, int limit) {
-        long long ans = 0;
-        for (int i=0; i<=min(limit, n); i++){
-            if (n-i > 2*limit)
-                continue;
-            ans += min(n-i, limit) - max(0, n-i-limit) + 1;
-        }  
+    long long cal(int x) {
+        if (x < 0) {
+            return 0;
+        }
+        return (long)x * (x - 1) / 2;
+    }
 
-        return ans;
+    long long distributeCandies(int n, int limit) {
+        return cal(n + 2) - 3 * cal(n - limit + 1) +
+               3 * cal(n - (limit + 1) * 2 + 2) - cal(n - 3 * (limit + 1) + 2);
     }
 };
