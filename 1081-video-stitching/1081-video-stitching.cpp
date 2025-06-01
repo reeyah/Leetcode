a44b90519cf75540cdc93fa0407568fe4748525a
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-01 22:59:44 +0530

    Time: 0 ms (100%), Space: 10.7 MB (41.42%) - LeetHub

---
+++ 1081-video-stitching/1081-video-stitching.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int videoStitching(vector<vector<int>>& clips, int time) {
+        vector<int>clipEnd(time+1, 0);
+        for(int i=0; i<clips.size(); i++) {
+            int start = clips[i][0];
+            int end = clips[i][1];
+            if(start>time)
+                continue;
+            clipEnd[start] = max(clipEnd[start], end);
+        }
+
+        int maxFar=0, currEnd=0, clipCount=0;
+
+        for(int t=0; t<=time; t++) {
+            if(currEnd == time)
+                return clipCount;
+            
+            if(t>currEnd)
+                return -1;
+            
+            maxFar = max(maxFar, clipEnd[t]);
+            if (t==currEnd) {
+                clipCount++;
+                currEnd = maxFar;
+            }
+        }
+        return clipCount;
+    }
+};
