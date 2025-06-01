c9ab3493cf67bbf2c500f7866d64819cca514768
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-01 22:31:35 +0530

    Time: 0 ms (100%), Space: 20.4 MB (84.63%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -3,7 +3,7 @@ public:
     int jump(vector<int>& nums) {
         int n = nums.size(), jumps = 0, maxFar = 0, curEnd = 0;
 
-        for(int i=0; i<n-1; i++) {
+        for(int i=0; i<=n-1; i++) {
             if (curEnd == n-1)
                 return jumps;
             /* Condition never occurs since TC says guaranteed to reach n-1 */
