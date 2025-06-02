d7f2c4d6571184e4deb91c913246995eb8c6f09d
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-02 10:13:35 +0530

    Time: 1 ms (36.28%), Space: 22.9 MB (43.5%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int>candies(n,0);
-        candies[0]=1, candies[n-1]=1;
+        vector<int>candies(n,1);
         for(int i=0; i<n-1; i++) {
             if (ratings[i] < ratings[i+1])
                 candies[i+1] = candies[i] + 1;
-            else candies[i+1] = 1;
         }
         for(int i=n-1; i>0; i--) {
             if ((ratings[i] < ratings[i-1])
