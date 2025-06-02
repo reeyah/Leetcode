e143f34546f8e556e7b5d88e4e123c5dd30261fe
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-02 10:11:42 +0530

    Time: 4 ms (17.74%), Space: 22.9 MB (59.49%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -9,24 +9,11 @@ public:
                 candies[i+1] = candies[i] + 1;
             else candies[i+1] = 1;
         }
-        for (int i =0 ; i<n; i++)
-            cout<<candies[i]<<" ";
-        cout<<"\n";
         for(int i=n-1; i>0; i--) {
             if ((ratings[i] < ratings[i-1])
                 && (candies[i-1] <= candies[i]))
                 candies[i-1] = candies[i]+1;
         }
-        for (int i =0 ; i<n; i++)
-            cout<<candies[i]<<" ";
-        // for(int i=1; i<n-1; i++) {
-        //     if(candies[i]-candies[i-1]>1 && candies[i]-candies[i+1]>1)
-        //         candies[i]--;
-        // }
-        // cout<<"\n";
-        // for (int i =0 ; i<n; i++)
-        //     cout<<candies[i]<<" ";
-
         return accumulate(candies.begin(), candies.end(), 0);
     }
 };
