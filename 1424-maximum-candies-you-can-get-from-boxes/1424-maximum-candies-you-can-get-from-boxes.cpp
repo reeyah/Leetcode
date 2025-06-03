d6807ac8a5340afb4ba149774d87663b8124137a
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-03 15:28:11 +0530

    Time: 0 ms (100%), Space: 44 MB (78.85%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        /* won't work if one box comes multiple times*/
         deque<int> dq;
 
         for(auto box: initialBoxes){
