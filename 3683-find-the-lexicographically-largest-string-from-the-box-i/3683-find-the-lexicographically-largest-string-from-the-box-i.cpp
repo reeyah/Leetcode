e0e9aafe684cf3dc0c06f5a94844d961baba4938
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-04 15:10:04 +0530

    Time: 0 ms (100%), Space: 11 MB (86.57%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    string answerString(string word, int numFriends) {
-        if (numFriends == 1)
-            return word;
-        int n = word.size(), length = n-numFriends+1, pos = 0;
-        vector<int> startPos;
-        for(int i=0; i<n; i++) {
-            if(word[pos] < word[i])
-                pos = i;
-            else if (word[pos] == word[i]) {
-                if (startPos.empty())
-                    startPos.push_back(i);
-                else if(word[startPos[0]] == word[pos])
-                    startPos.push_back(i);
-                else {
-                    startPos.clear();
-                    startPos.push_back(i);
-                }
+    string lastSubstring(string s) {
+        int i = 0, j = 1, n = s.size();
+        while (j < n) {
+            int k = 0;
+            while (j + k < n && s[i + k] == s[j + k]) {
+                k++;
+            }
+            if (j + k < n && s[i + k] < s[j + k]) {
+                int t = i;
+                i = j;
+                j = max(j + 1, t + k + 1);
+            } else {
+                j = j + k + 1;
             }
         }
+        return s.substr(i, n - i);
+    }
 
-        if(word[startPos[0]] != word[pos])
-            startPos.clear();
-        
-        string res = word.substr(pos,length);
-        if (startPos.empty())
-            return res;
-
-        for(int i = 0; i<startPos.size(); i++) {
-            int tempPos = startPos[i];
-            string tempRes = word.substr(tempPos,length);
-            res = max(res, tempRes);
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) {
+            return word;
         }
-
-        return res;
+        string last = lastSubstring(word);
+        int n = word.size(), m = last.size();
+        return last.substr(0, min(m, n - numFriends + 1));
     }
 };
