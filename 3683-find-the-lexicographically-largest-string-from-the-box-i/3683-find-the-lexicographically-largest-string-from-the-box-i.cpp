308bd682b8b195647e154e7d42ed567eb2b111b0
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-04 15:15:35 +0530

    Time: 13 ms (87.31%), Space: 36.9 MB (78.36%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    string lastSubstring(string s) {
-        int i = 0, j = 1, n = s.size();
-        while (j < n) {
-            int k = 0;
-            while (j + k < n && s[i + k] == s[j + k]) {
-                k++;
-            }
-            if (j + k < n && s[i + k] < s[j + k]) {
-                int t = i;
-                i = j;
-                j = max(j + 1, t + k + 1);
-            } else {
-                j = j + k + 1;
-            }
-        }
-        return s.substr(i, n - i);
-    }
-
     string answerString(string word, int numFriends) {
-        if (numFriends == 1) {
+        if (numFriends == 1)
             return word;
+        int n = word.size(), length = n-numFriends+1;
+        char ch = *max_element(word.begin(), word.end());
+        vector<int> startPos;
+        for(int i=0; i<n; i++) {
+            if (word[i]==ch)
+                startPos.push_back(i);
         }
-        string last = lastSubstring(word);
-        int n = word.size(), m = last.size();
-        return last.substr(0, min(m, n - numFriends + 1));
+        
+        string res = length + startPos[0] > n ? word.substr(startPos[0]) : word.substr(startPos[0],length);
+        if (startPos.size()==1)
+            return res;
+
+        for(int i = 0; i<startPos.size(); i++) {
+            int tempPos = startPos[i];
+            string tempRes = length + tempPos > n ? word.substr(tempPos) : word.substr(tempPos,length);
+            res = max(res, tempRes);
+        }
+
+        return res;
     }
 };
