6499ab74cf8389bce6360815b534137be22c212e
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-06 20:14:42 +0530

    Time: 84 ms (26.73%), Space: 33.8 MB (56.93%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     string robotWithString(string s) {
-        unordered_map<char, int> cnt;
-        for (char c : s) {
-            cnt[c]++;
-        }
-
+        vector<int>cnt(26, 0);
+        for(char c: s)
+            cnt[c-'a']++;
+        int minCharacter = 0;
         stack<char> stk;
         string res;
-        char minCharacter = 'a';
         for (char c : s) {
             stk.emplace(c);
-            cnt[c]--;
-            while (minCharacter != 'z' && cnt[minCharacter] == 0) {
+            cnt[c-'a']--;
+            while (minCharacter != 25 && cnt[minCharacter] == 0) {
                 minCharacter++;
             }
-            while (!stk.empty() && stk.top() <= minCharacter) {
+            while (!stk.empty() && stk.top()-'a' <= minCharacter) {
                 res.push_back(stk.top());
                 stk.pop();
             }
