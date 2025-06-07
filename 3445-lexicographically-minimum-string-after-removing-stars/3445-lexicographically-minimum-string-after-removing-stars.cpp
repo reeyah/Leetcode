182a5309f6f1b36b6435d7128d2e815cfd8532b7
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-07 09:56:36 +0530

    Time: 122 ms (61.69%), Space: 24.6 MB (85.71%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     string clearStars(string s) {
         int n = s.size();
-        vector<bool>v(n, true);
         priority_queue<char, vector<char>, greater<char>> pq; //Min-heap
         unordered_map<char, vector<int>>m;
 
         for(int i=0; i<n; i++) {
             if(s[i]=='*') {
                 char smallest = pq.top();
+                pq.pop();
                 int index = m[smallest].back();
                 m[smallest].pop_back(); //removing last index
-                v[i]=false;
-                v[index]=false;
+                s[index]='*';
             } else {
                 pq.push(s[i]);
                 m[s[i]].push_back(i);
@@ -21,7 +20,7 @@ public:
 
         string res = "";
         for(int i=0; i<n; i++){
-            if(v[i])
+            if(s[i]!='*')
                 res+=s[i];
         }
 
