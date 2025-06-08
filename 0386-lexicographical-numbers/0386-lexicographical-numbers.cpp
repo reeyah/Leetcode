64f5369c027f104ebe38d774c323092743d59d79
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-08 19:51:11 +0530

    Time: 1 ms (58.46%), Space: 15.2 MB (24.83%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    vector<int> lexicalSorted;
+public:
+    vector<int> lexicalOrder(int n) {
+        for (int i=1; i<=9; i++) {
+            dfs(i, n);
+        }
+
+        return lexicalSorted;
+    }
+private:
+    void dfs(int cur, int n) {
+        if(cur>n)
+            return;
+        
+        lexicalSorted.push_back(cur);
+        for(int dig =0; dig<=9; dig++) {
+            int next = cur*10+dig;
+            // if(next>n)
+            //     break; 
+            dfs(next, n);
+        }
+    }
+};
