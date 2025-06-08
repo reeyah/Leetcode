a55f4e15fe97343f678bc6ca571e6ad57f854634
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-08 23:30:58 +0530

    Time: 0 ms (100%), Space: 8.7 MB (60.89%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -7,16 +7,19 @@ public:
 		if(n==0 || n==1) // 0 non-constraint, if str.size == 1 then just 1 way to decode
             return 1;   
 		
-        vector<int>dp(n+1);
+        int prev2 = 1, prev1 = 1; //base
 		
-        dp[0]=dp[1]=1; //base
+        //dp[0]=dp[1]=1; //base
 		for(int i=2; i<=n; i++){
-		    if(str[i-1]!='0')
-		        dp[i]+=dp[i-1];
-		     if(str[i-2]!='0') // >=10
+            int curr = 0;
+		    if(str[i-1]!='0') // if num to be added is 0 it wont stand on its own and without pair will eventually result in 0 possible decodings
+		        curr = prev1;
+		    if(str[i-2]!='0') // >=10
 		        if(stoi(str.substr(i-2,2)) <= 26)
-		            dp[i]+=dp[i-2];
+		            curr += prev2;
+            prev2 = prev1;
+            prev1 = curr;
 		}
-		return dp[n];
+		return prev1;
     }
 };
