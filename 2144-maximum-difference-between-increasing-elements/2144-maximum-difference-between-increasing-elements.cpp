5d9ed58cd4484324ef9e471d0f70269c1ae94af3
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-16 14:35:09 +0530

    Time: 0 ms (100%), Space: 12 MB (38.11%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -4,20 +4,11 @@ public:
         int res = -1;
 
         for(int i=0, j=i+1; j<nums.size() && i<j; j++) {
-            while(nums[i] >= nums[j]) {
+            if(nums[i] >= nums[j]) 
                 i=j;
-                if (++j >= nums.size()) 
-                    break;
-            }
-
-            if (j >= nums.size()) 
-                    break;
-
-            cout<<nums[i]<<" "<<nums[j]<<endl;
-
-            res = max(res, nums[j]-nums[i]);
+            else 
+                res = max(res, nums[j]-nums[i]);   
         }
-
         return res;
     }
 };
