b318d449125b56cff8dfd3d3b024fdacc9a0ca95
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-17 01:09:36 +0530

    Time: 367 ms (48.57%), Space: 45.9 MB (75.43%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.cpp
@@ -1,41 +1,3 @@
-class DSU {
-    vector<int> parent, rank;
-public:
-    DSU(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-        for (int i = 0; i < n; i++) 
-            parent[i] = i;
-    }
-
-    int find(int x) {
-        if(parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    bool Union(int ptA, int ptB) {
-        int x = find(ptA);
-        int y = find(ptB);
-
-        if(x==y)
-            return false; //pts A, B already connected
-        
-        if(rank[x] > rank[y])
-            parent[y] = x;
-        
-        else if(rank[y] > rank[x])
-            parent[x] = y;
-
-        else {
-            parent[y] = x;
-            rank[x]++;
-        }
-
-        return true;
-    }
-};
-
 class Solution {
 private:
     int dist(vector<int> &ptA, vector<int> &ptB) {
@@ -43,34 +5,33 @@ private:
     }
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<vector<int>>graph;
+        int n = points.size();
+        vector<bool>visited(n, false);
 
-        for(int i=0; i<points.size(); i++) {
-            for(int j=i+1; j<points.size(); j++) {
-                graph.push_back({i, j, dist(points[i], points[j])}); 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>>pq; //min heap
+
+        int distance = 0;
+
+        pq.push({0, 0});
+
+        while(!pq.empty()) {
+            auto [w, u] = pq.top();
+            pq.pop();
+
+            if(visited[u])
+                continue;
+            
+            visited[u] = true;
+            distance += w;
+
+            for(int v = 0; v < n ; v++) {
+                if(!visited[v]) {
+                    int d = dist(points[u], points[v]);
+                    pq.push({d, v});
+                }
             }
         }
 
-        DSU dsu(points.size());
-        int distance = 0, count=0;
-
-        sort(graph.begin(), graph.end(), [](auto &a, auto &b) {
-            return a[2] < b[2];
-        });
-
-        // for(auto g:graph) {
-        //     cout<<g[0]<<" "<<g[1]<<" "<<g[2]<<endl;
-        // }
-
-        for(auto g: graph) {
-            if(dsu.Union(g[0], g[1])) {
-                distance += g[2];
-                count++;
-            }
-            if(count == points.size()-1)
-                break;
-        }
-
-        return count == points.size()-1 ? distance : -1;
+        return distance;
     }
 };
