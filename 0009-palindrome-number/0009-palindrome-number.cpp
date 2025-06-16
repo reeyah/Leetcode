cca673b4bd1c2bd02115512bf7b50d342126a305
Reeya Hazarika <55135851+reeyah@users.noreply.github.com>
2025-06-16 22:13:51 +0530

    Time: 3 ms (46.89%), Space: 10.8 MB (16.46%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0)
-            return false;
-        if (x == 0)
-            return true;
-        if (x % 10 == 0)
-            return false;
+        // if (x < 0)
+        //     return false;
+        // if (x == 0)
+        //     return true;
+        // if (x % 10 == 0)
+        //     return false;
             
-        int reverseHalf = 0;
-        while(x > reverseHalf) {
-            reverseHalf = reverseHalf * 10 + x % 10;
-            x /= 10;
+        // int reverseHalf = 0;
+        // while(x > reverseHalf) {
+        //     reverseHalf = reverseHalf * 10 + x % 10;
+        //     x /= 10;
+        // }
+
+        // //cout<<x<<" "<<reverseHalf<<endl;
+
+        // return x == reverseHalf || x == reverseHalf/10;
+
+        string num = to_string(x);
+
+        for(int i=0, j=num.size()-1; i<j; i++, j--) {
+            if(num[i] != num[j])
+                return false;
         }
 
-        //cout<<x<<" "<<reverseHalf<<endl;
-
-        return x == reverseHalf || x == reverseHalf/10;
+        return true;
     }
 };
